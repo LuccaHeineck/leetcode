92109e7a7201bcf4c22ecf2e82c7d89821baf2d3
LuccaHeineck <luccacheineck@gmail.com>
2026-09-10 17:36:08 -0300

    solved: 0004 - median two sorted in O((m+n) log(m+n))

--- 0004-HAR-median-two-sorted-arrays/solution.java
+++ 0004-HAR-median-two-sorted-arrays/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // first merge both, then get middle number
-        int[] merged = IntStream.concat(IntStream.of(nums1), IntStream.of(nums2))
-            .toArray();
+        int[] merged = IntStream.concat(IntStream.of(nums1), IntStream.of(nums2)).toArray();
+        Arrays.sort(merged);
 
         int size = merged.length;
-
         if (size % 2 == 0) {
-            return merged[size / 2];
+            return (merged[size / 2 - 1] + (long) merged[size / 2]) / 2.0;
         } else {
-            int mid1 = merged[(size / 2) - 1];
-            int mid2 = merged[size / 2];
-            return (double) (mid1 + mid2) / 2.0;
+            return merged[size / 2];
         }
     }
 }
